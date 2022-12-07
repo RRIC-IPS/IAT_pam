be003628293cc09eb146218fcd46a0f3361577ab
RRIC-IPS <118233825+RRIC-IPS@users.noreply.github.com>
2022-12-07 18:44:02 +0800

    Update testIAT.js

--- testIAT.js
+++ testIAT.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://rric-ips.github.io/IAT_pam/IATAPI.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://RRIC-IPS.github.io/IAT_pam/IATAPI.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
